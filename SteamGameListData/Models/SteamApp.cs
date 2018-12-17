51be14fcca78114b68fd0dcacba25aaa56242898
Dan Stow <Dan_Stow@live.co.uk>
2018-12-17 20:56:17 +0000

    Add methods for creating and getting Steam Apps

--- SteamGameListData/Models/SteamApp.cs
+++ SteamGameListData/Models/SteamApp.cs
@@ -11,5 +11,27 @@ namespace SteamGameListData.Models
         public int SteamAppId { get; set; }
         public int AppId { get; set; }
         public string Name { get; set; }
+
+        public static int CreateSteamApp(int appId, string name)
+        {
+            SteamApp newApp = new SteamApp()
+            {
+                AppId = appId,
+                Name = name
+            };
+
+            AppContext context = new AppContext();
+            context.SteamApps.Add(newApp);
+            context.SaveChanges();
+
+            return newApp.SteamAppId;
+        }
+
+        public static List<SteamApp> GetAllSteamApps()
+        {
+            AppContext context = new AppContext();
+
+            return context.SteamApps.ToList();
+        }
     }
 }
