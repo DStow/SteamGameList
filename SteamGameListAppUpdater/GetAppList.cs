222eb248bf08785494ecea72b1675b4fe687fad8
Dan Stow <Dan_Stow@live.co.uk>
2018-12-17 21:18:42 +0000

    Add class for retrieving app list from Steam

---
+++ SteamGameListAppUpdater/GetAppList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace SteamGameListAppUpdater
+{
+    internal static class GetAppList
+    {
+        public static List<GetAppListApp> GetAllApps()
+        {
+            List<GetAppListApp> results = new List<GetAppListApp>();
+
+            RestClient client = new RestClient("http://api.steampowered.com");
+            RestRequest request = new RestRequest("/ISteamApps/GetAppList/v0002/", Method.GET);
+
+            request.Parameters.Add(new Parameter("key", ConfigurationManager.AppSettings["SteamApiKey"], ParameterType.QueryString));
+
+            IRestResponse response = client.Execute(request);
+
+            dynamic allAppData = JsonConvert.DeserializeObject(response.Content);
+
+            foreach (var app in allAppData["applist"]["apps"])
+            {
+                GetAppListApp newApp = new GetAppListApp()
+                {
+                    AppId = app.appid,
+                    Name = app.name
+                };
+
+                results.Add(newApp);
+            }
+
+            return results;
+        }
+    }
+
+    internal class GetAppListApp
+    {
+        public int AppId { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return AppId + ": " + Name;
+        }
+    }
+}
--- SteamGameListAppUpdater/Program.cs
+++ SteamGameListAppUpdater/Program.cs
@@ -10,6 +10,18 @@ namespace SteamGameListAppUpdater
     {
         static void Main(string[] args)
         {
+            GetAppList.GetAllApps();
+            // Connect to the API and get all of the apps.
+
+            // For each app check if it exists in the database already
+
+            // If it doesn't, add to list to check back later
+
+            // Loop through list and go and retrieve more information until hit API call limit
+
+            // Once hit API call limit wait for 2 minutes and try again until we are up and running again
+
+            // Display progress to the user
         }
     }
 }
